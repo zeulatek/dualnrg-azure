4da1be0173552cb3f8edfe16104082c571caa97c
Roger Best <rbest@microsoft.com>
2017-09-15 16:38:50 -0700

    Updated and cleaned up templates.  Added script to create environment in lab.  TODO cleanup and file location for  script.

---
+++ devtestlabs/Scripts/CreateEnvironmentInLab.ps1
@@ -0,0 +1,19 @@
+
+$deployName = "S4HanaDemoDeployment"
+$updatedTemplateFilePath = "environmentdeploy.json"
+$DevTestLabName = "S4Hana-dtl"
+$machineUserName = "testuser"
+$machinePassword = ConvertTo-SecureString 'P@ss1worD' -AsPlainText -Force
+$subId = "b47e2e67-6ce6-430d-b41a-c439e1ff22fb"
+
+Login-AzureRmAccount
+
+Set-AzureRmContext -SubscriptionId $subId | Out-Null
+
+$ResourceGroupName = (Find-AzureRmResource -ResourceType 'Microsoft.DevTestLab/labs' | Where-Object { $_.Name -eq $DevTestLabName}).ResourceGroupName
+
+Write-Output "Starting Environment creation. $(Get-Date)"
+
+$vmDeployResult = New-AzureRmResourceGroupDeployment -Name $deployName -ResourceGroupName $ResourceGroupName -TemplateFile $updatedTemplateFilePath -labName $DevTestLabName -armParaName $machineUserName -armParaPassword $machinePassword
+
+Write-Output "Environment creation completed. $(Get-Date)"
