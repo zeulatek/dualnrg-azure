e6d63f2565b3e43044f342d12a5119a1e5965574
Roger Best <rbest@microsoft.com>
2017-09-18 14:35:21 -0700

    Added download from repo

--- devtestlabs/Scripts/CreateEnvironmentInLab.ps1
+++ devtestlabs/Scripts/CreateEnvironmentInLab.ps1
@@ -1,12 +1,16 @@
 
 $deployName = "S4HanaDemoDeployment"
-$updatedTemplateFilePath = "https://github.com/zeulatek/dualnrg-azure/blob/master/devtestlabs/Scripts/environmentdeploy.json"
+$updatedTemplateFilePath = "c:\ignitedemo\environmentdeploy.json"
 $DevTestLabName = "S4Hana-dtl"
 $machineUserName = "testuser"
 $machinePassword = ConvertTo-SecureString 'P@ss1worD' -AsPlainText -Force
 $subId = "b47e2e67-6ce6-430d-b41a-c439e1ff22fb"
 
 Login-AzureRmAccount
+
+New-Item -Path "C:\ignitedemo" -ItemType Directory -Force
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/zeulatek/dualnrg-azure/master/devtestlabs/Scripts/environmentdeploy.json" -OutFile $updatedTemplateFilePath
 
 Set-AzureRmContext -SubscriptionId $subId | Out-Null
 
